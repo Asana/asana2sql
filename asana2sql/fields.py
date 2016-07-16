2c93b0ff9888d361a2950d9afe1ab4c3d215004c
Drew Haven <drewhaven@asana.com>
2016-07-15 17:55:33 -0700

    Adds custom field support. TODO: Code needs some clean up, e.g. consistent queries.

--- asana2sql/fields.py
+++ asana2sql/fields.py
@@ -97,6 +97,35 @@ class ProjectsField(Field):
         for project_id in stale_project_ids:
             self._workspace.remove_task_from_project(task["id"], project_id)
 
+
+class CustomFields(Field):
+    def __init__(self, workspace):
+        super(CustomFields, self).__init__(None, None)
+        self._workspace = workspace
+
+    def required_fields(self):
+        # NB: There's a bug in the API as of this writing such that if you
+        # attempt to index fields inside the custom fields then you'll just get
+        # back a list of null values instead of the requested ones.  Thus we
+        # just have to request it all.
+        return ["id", "custom_fields"]
+
+    def get_data_from_task(self, task):
+        custom_fields = task.get("custom_fields", [])
+
+        new_field_ids = set(custom_field["id"]
+                            for custom_field in custom_fields)
+        old_field_ids = set(self._workspace.task_custom_field_values(task["id"]))
+        stale_field_ids = old_field_ids.difference(new_field_ids)
+
+        for custom_field in custom_fields:
+            self._workspace.add_custom_field_value(task["id"], custom_field)
+
+        for field_id in stale_field_ids:
+            self._workspace.remove_custom_field_value(
+                    task["id"], custom_field_id)
+
+
 def default_fields(workspace):
     return [TaskIdPrimaryKeyField(),
             NameField(),
@@ -112,5 +141,6 @@ def default_fields(workspace):
             AssigneeStatus(),
             ProjectsField(workspace),
             ParentIdField(),
+            CustomFields(workspace),
             ]
 
--- asana2sql/workspace.py
+++ asana2sql/workspace.py
@@ -30,7 +30,7 @@ CREATE_USERS_TABLE = (
         name VARCHAR(1024));
         """)
 INSERT_USER = (
-        """INSERT OR REPLACE INTO "{table_name}" VALUES (?, ?)""")
+        """INSERT OR REPLACE INTO "{table_name}" VALUES (?, ?);""")
 
 FOLLOWERS_TABLE_NAME = "followers"
 CREATE_FOLLOWERS_TABLE = (
@@ -45,28 +45,45 @@ CREATE_CUSTOM_FIELDS_TABLE = (
         """CREATE TABLE IF NOT EXISTS "{table_name}" (
         id INTEGER NOT NULL PRIMARY KEY,
         name VARCHAR(1024),
-        type INTEGER NOT NULL)
+        type INTEGER NOT NULL);
         """)
+INSERT_CUSTOM_FIELD = (
+        """INSERT OR REPLACE INTO "{table_name}" VALUES (?, ?, ?);""")
 
 CUSTOM_FIELD_ENUM_VALUES_TABLE_NAME = "custom_field_enum_values"
 CREATE_CUSTOM_FIELD_ENUM_VALUES_TABLE = (
         """CREATE TABLE IF NOT EXISTS "{table_name}" (
-        id INTEGER NOT NULL PRIMARY KEY,
+        custom_field_id INTEGER NOT NULL,
+        id INTEGER NOT NULL,
         name VARCHAR(1024),
         enabled BOOLEAN NOT NULL,
-        color VARCHAR(64) NOT NULL)
+        color VARCHAR(64) NOT NULL,
+        PRIMARY KEY (custom_field_id, id));
         """)
+SELECT_CUSTOM_FIELD_ENUM_VALUES_FOR_CUSTOM_FIELD = (
+        """SELECT id FROM {table_name} WHERE custom_field_id = ?;""")
+INSERT_CUSTOM_FIELD_ENUM_VALUE = (
+        """INSERT OR REPLACE INTO "{table_name}" VALUES (?, ?, ?, ?, ?);""")
+DELETE_CUSTOM_FIELD_ENUM_VALUE = (
+        """DELETE FROM "{table_name}" WHERE id = ?;""")
 
 CUSTOM_FIELD_VALUES_TABLE_NAME = "custom_field_values"
 CREATE_CUSTOM_FIELD_VALUES_TABLE = (
         """CREATE TABLE IF NOT EXISTS "{table_name}" (
-        id INTEGER NOT NULL,
         task_id INTEGER NOT NULL,
+        custom_field_id INTEGER NOT NULL,
         text_value TEXT,
         number_value FLOAT,
         enum_value INTEGER,
-        PRIMARY KEY (id, task_id))
+        PRIMARY KEY (task_id, custom_field_id));
         """)
+
+SELECT_CUSTOM_FIELD_VALUES_FOR_TASK = (
+        "SELECT custom_field_id FROM {table_name} WHERE task_id = ?;")
+INSERT_CUSTOM_FIELD_VALUE = (
+        "INSERT OR REPLACE INTO {table_name} VALUES (?, ?, ?, ?, ?);")
+DELETE_CUSTOM_FIELD_VALUE = (
+        "DELETE FROM {table_name} WHERE task_id = ? AND custom_field_id = ?;")
 
 class Workspace(object):
     """Abstraction around all the supporting values for a project that are
@@ -124,18 +141,19 @@ class Workspace(object):
                 CREATE_CUSTOM_FIELD_VALUES_TABLE.format(
                     table_name=self.custom_field_values_table_name()))
 
-    def ensure_user_exists(self, user):
+    def add_user(self, user):
         self._db_client.write(
                 INSERT_USER.format(
                     table_name=self.users_table_name()),
                 (user["id"], user["name"]))
 
-    def ensure_project_exists(self, project):
+    def add_project(self, project):
         self._db_client.write(
                 INSERT_PROJECT.format(
                     table_name=self.projects_table_name()),
                 (project["id"], project["name"]))
 
+    # Task Membership
     def task_memberships(self, task_id):
         return [row[0] for row in self._db_client.read(
                 SELECT_PROJECT_MEMBERSHIPS.format(
@@ -143,7 +161,7 @@ class Workspace(object):
                 task_id)]
 
     def add_task_to_project(self, task_id, project):
-        self.ensure_project_exists(project)
+        self.add_project(project)
         self._db_client.write(
                 INSERT_PROJECT_MEMBERSHIP.format(
                     table_name=self.project_memberships_table_name()),
@@ -155,3 +173,83 @@ class Workspace(object):
                     table_name=self.project_memberships_table_name()),
                 (task_id, project_id))
 
+    # Custom fields
+    def add_custom_field(self, custom_field_value):
+        """Adds a custom field to the database.
+
+        NB: This depends on the data for the custom field being available in
+        the custom_field_value parameter, which is true if custom_fields are
+        fetched via the task API.
+        """
+        self._db_client.write(
+                INSERT_CUSTOM_FIELD.format(
+                    table_name=self.custom_fields_table_name()),
+                custom_field_value["id"],
+                custom_field_value["name"],
+                custom_field_value["type"]);
+
+        if custom_field_value["type"] == "enum":
+            self.add_custom_field_enum_values(custom_field_value["id"])
+
+    def get_custom_field(self, custom_field_id):
+        # NB: The python client doesn't support custom fields yet, so we have
+        # to fetch manually.
+        return self._asana_client.get("/custom_fields/{}".format(custom_field_id), "")
+
+    # Custom field enum values
+    def custom_field_enum_values(self, custom_field_id):
+        return [row[0] for row in
+                self._dbclient.read(
+                    SELECT_CUSTOM_FIELD_ENUM_VALUES_FOR_CUSTOM_FIELD.format(
+                        table_name=self.custom_field_enum_values_table_name()),
+                    custom_field_id)]
+
+    def add_custom_field_enum_values(self, custom_field_id):
+        custom_field_def = self.get_custom_field(custom_field_id)
+
+        old_ids = custom_field_enum_values(custom_field_id)
+        new_ids = [option["id"]
+                   for option in custom_field_def.get("enum_options", [])]
+
+        for enum_option in custom_field_def.get("enum_options", []):
+            self._db_client.write(
+                    INSERT_CUSTOM_FIELD_ENUM_VALUE.format(
+                        table_name=self.custom_field_enum_values_table_name()),
+                    custom_field_id,
+                    enum_option["id"],
+                    enum_option["name"],
+                    enum_option["enabled"],
+                    enum_option["color"])
+        for id in old_ids:
+            self._db_client.write(
+                    DELETE_CUSTOM_FIELD_ENUM_VALUE.format(
+                        table_name=self.custom_field_enum_values_table_name()),
+                    id)
+
+    # Custom field values
+    def task_custom_field_values(self, task_id):
+        return [row[0] for row in
+                self._db_client.read(
+                    SELECT_CUSTOM_FIELD_VALUES_FOR_TASK.format(
+                        table_name=self.custom_field_values_table_name()),
+                    task_id)]
+
+    def add_custom_field_value(self, task_id, custom_field):
+        self.add_custom_field(custom_field)
+        self._db_client.write(
+                INSERT_CUSTOM_FIELD_VALUE.format(
+                    table_name=self.custom_field_values_table_name()),
+                task_id,
+                custom_field["id"],
+                custom_field.get("text_value"),
+                custom_field.get("number_value"),
+                custom_field.get("enum_value") and
+                    custom_field.get("enum_value").get("id"))
+
+
+    def remove_custom_field_value(self, task_id, custom_field_id):
+        self._db_client.write(
+                DELETE_CUSTOM_FIELD_VALUE.format(
+                    table_name=self.custom_field_values_table_name()),
+                task_id,
+                custom_field_id)
